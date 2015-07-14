8ef85fc737ebfbf7bec431e04857ef4da894b56b
Daniel Kelley <dan@kelleyland.com>
2015-07-14 19:24:27 -0400

    updated scores

--- data/thursday.js
+++ data/thursday.js
@@ -19,18 +19,18 @@
 ["Smokey Joes","LaSalle Alums","6:30 PM"]
 ]},
 {"Date":"06/25/15","Games":[
-["1789","Dunn Financial","6:30 PM"],
-["Bryn Mawr Trust","GMH","6:30 PM"],
+["1789","Dunn Financial","6:30 PM",9,7],
+["Bryn Mawr Trust","GMH","6:30 PM",0,0],
 ["Mojo’s","Mesa Fresh","6:30 PM",8,18],
-["LaSalle Alums","Smokey Joes","8:30 PM"],
-["Lacrosse International","OPC","6:30 PM"]
+["LaSalle Alums","Smokey Joes","8:30 PM",0,0],
+["Lacrosse International","OPC","6:30 PM",0,0]
 ]},
 {"Date":"07/09/15","Games":[
 ["GMH","Smokey Joes","8:30 PM",0,0],
 ["Bryn Mawr Trust","Mesa Fresh","6:30 PM",8,6],
-["Mojos","Dunn Financial","6:30 PM",0,0],
+["Mojo’s","Dunn Financial","6:30 PM",0,0],
 ["Lacrosse International","LaSalle Alums","6:30 PM",0,0],
-["1789","OPC","6:30 PM",0,0]
+["1789","OPC","6:30 PM",9,5]
 ]},
 {"Date":"07/16/15","Games":[
 ["Bryn Mawr Trust","OPC","6:30 PM"],
--- data/tuesday.js
+++ data/tuesday.js
@@ -2,8 +2,8 @@
 "games":
 [
 {"Date":"06/16/15","Games":[
-["Sun Valley Cocos","O’Hara","6:30 PM",0,0],
-["Top Shelf","Garnet Valley","6:30 PM",0,0],
+["Sun Valley Cocos","O’Hara","6:30 PM",14,3],
+["Top Shelf","Garnet Valley","6:30 PM",4,20],
 ["Ridley ","Haverford","6:30 PM",0,0],
 ["Springfield","Upper Darby","6:30 PM",6,8],
 ["Lax Zone","Old Lions","6:30 PM",15,7],
@@ -16,7 +16,7 @@
 ["Upper Darby","Ridley ","6:30 PM",0,0],
 ["Top Shelf","Haverford","8:30 PM",0,0],
 ["Garnet Valley","O’Hara","6:30 PM",0,0],
-["Old Lions","Sun Valley Cocos","6:30 PM",0,0],
+["Old Lions","Sun Valley Cocos","6:30 PM",10,11],
 ["Springfield","Rustin","6:30 PM",0,0],
 ["Mud Dogs","Kelly’s Sports","6:30 PM",0,0],
 ["RJP Consulting","Duffers Pub","8:00 PM",0,0],
@@ -26,7 +26,7 @@
 {"Date":"06/30/15","Games":[
 ["Old Lions","Top Shelf","6:30 PM",12,4],
 ["Sun Valley Cocos","Ridley ","6:30 PM",0,0],
-["Garnet Valley","Springfield","6:30 PM",0,0],
+["Garnet Valley","Springfield","6:30 PM",7,4],
 ["Haverford","Upper Darby","6:30 PM",0,0],
 ["Dunn Financial","O’Hara","6:30 PM",15,3],
 ["Lax Zone","Duffers Pub","8:00 PM",15,7],
@@ -36,11 +36,11 @@
 ]},
 {"Date":"07/07/15","Games":[
 ["Haverford","Springfield","6:30 PM",0,0],
-["Ridley ","Garnet Valley","6:30 PM",0,0],
-["Sun Valley Cocos","Top Shelf","6:30 PM",0,0],
+["Ridley ","Garnet Valley","6:30 PM",12,10],
+["Sun Valley Cocos","Top Shelf","6:30 PM",12,4],
 ["O’Hara","Old Lions","6:30 PM",5,19],
 ["Mud Dogs","Upper Darby","6:30 PM",10,9],
-["RJP Consulting","Rustin","6:30 PM",0,0],
+["RJP Consulting","Rustin","6:30 PM",16,6],
 ["Baggataway Tav","Kelly’s Sports","6:30 PM",0,0],
 ["Great Am LC ","Duffers Pub","8:00 PM",0,0],
 ["Dunn Financial","Lax Zone","6:30 PM",5,10]
