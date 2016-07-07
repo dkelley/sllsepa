9cb1fa2100a5393e943f163bc58476597c55caee
Daniel Kelley <dan@kelleyland.com>
2016-07-07 00:09:07 -0400

    remove unused templates and updated contacts

--- data/tuesdayDelco.js
+++ data/tuesdayDelco.js
@@ -12,20 +12,20 @@
 [
 {"Date":"06/14/16","Games":[
 ["COCOS","O’Hara","6:30 PM",0,0],
-["Haverford","Upper Darby","6:30 PM",0,20],
+["Haverford","Upper Darby","6:30 PM",16,3],
 ["Springfield","HAVEN","6:30 PM",0,0],
 ["Ridley","Old Lions","6:30 PM",0,0]
 ]},
 {"Date":"06/21/16","Games":[
 ["Haverford","COCOS","6:30 PM",0,0],
-["HAVEN","O’Hara","6:30 PM",0,20],
+["HAVEN","O’Hara","6:30 PM",5,6],
 ["Old Lions","Upper Darby","6:30 PM",0,0],
 ["Ridley","Springfield","6:30 PM",0,0]
 ]},
 {"Date":"06/28/16","Games":[
 ["COCOS","HAVEN","6:30 PM",0,0],
 ["O’Hara","Ridley","6:30 PM",0,20],
-["Old Lions","Haverford","7:30 PM",0,0],
+["Old Lions","Haverford","7:30 PM",16,3],
 ["Upper Darby","Springfield","6:30 PM",0,0]
 ]},
 {"Date":"07/05/16","Games":[
--- data/tuesdayWest.js
+++ data/tuesdayWest.js
@@ -12,21 +12,21 @@
 [
 {"Date":"06/14/16","Games":[
 ["Dunn Financial","Baggataway Tav","6:30 PM",0,0],
-["Rustin","Duffers Pub","8:00 PM",0,0],
-["Garnet Valley","PJ Whelihans","6:30 PM",0,0],
+["Rustin","Duffers Pub","8:00 PM",1,20],
+["Garnet Valley","PJ Whelihans","6:30 PM",7,6],
 ["Great American Pub","Kelly’s Sports","6:30 PM",6,8]
 ]},
 {"Date":"06/21/16","Games":[
-["Baggataway Tav","Duffers Pub","8:00 PM",0,0],
+["Baggataway Tav","Duffers Pub","8:00 PM",8,10],
 ["Garnet Valley","Dunn Financial","8:00 PM",0,0],
 ["Rustin","Great American Pub","6:30 PM",0,0],
-["PJ Whelihans","Kelly’s Sports","6:30 PM",6,8]
+["PJ Whelihans","Kelly’s Sports","6:30 PM",15,6]
 ]},
 {"Date":"06/28/16","Games":[
-["Baggataway Tav","Garnet Valley","6:30 PM",0,0],
-["Great American Pub","Duffers Pub","8:00 PM",0,0],
+["Baggataway Tav","Garnet Valley","6:30 PM",3,7],
+["Great American Pub","Duffers Pub","8:00 PM",0,1],
 ["Dunn Financial","Kelly’s Sports","6:30 PM",0,0],
-["PJ Whelihans","Rustin","6:30 PM",0,0]
+["PJ Whelihans","Rustin","6:30 PM",18,2]
 ]},
 {"Date":"07/05/16","Games":[
 ["Great American Pub","Baggataway Tav","6:30 PM",0,0],
