67fa4b0fa4d0fc13cb47c916ce4c0388aa1140ca
Daniel Kelley <dan@kelleyland.com>
2016-07-07 00:20:35 -0400

    latest scores

--- data/tuesdayWest.js
+++ data/tuesdayWest.js
@@ -18,7 +18,7 @@
 ]},
 {"Date":"06/21/16","Games":[
 ["Baggataway Tav","Duffers Pub","8:00 PM",8,10],
-["Garnet Valley","Dunn Financial","8:00 PM",0,0],
+["Garnet Valley","Dunn Financial","8:00 PM",13,10],
 ["Rustin","Great American Pub","6:30 PM",0,0],
 ["PJ Whelihans","Kelly’s Sports","6:30 PM",15,6]
 ]},
@@ -30,7 +30,7 @@
 ]},
 {"Date":"07/05/16","Games":[
 ["Great American Pub","Baggataway Tav","6:30 PM",0,0],
-["Kelly’s Sports","Garnet Valley","6:30 PM",0,0],
+["Kelly’s Sports","Garnet Valley","6:30 PM",3,15],
 ["PJ Whelihans","Duffers Pub","8:00 PM",0,0],
 ["Rustin","Dunn Financial","6:30 PM",0,0]
 ]},
