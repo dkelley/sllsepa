a0e56fae15a685a0c1c1daeed06b0b611c9c81f4
roseyderoser <roseyderoser@yahoo.com>
2016-07-11 20:10:14 -0400

    Update to Thursday 16
    Updated Thursday night schedule to 2016

--- data/thursday.js
+++ data/thursday.js
@@ -1,56 +1,61 @@
 {"teams":[
-	["Bryn Mawr Trust","West"],
-	["Dunn Financial","West"],
-	["GMH","West"],
-	["LaSalle Alums","East"],
-	["Lacrosse International","East"],
-	["Mesa Fresh","West"],
+	["1789 GERMANTOWN ACADEMY" "West"],
+	["Bryn Mawr Trust","East"],
+	["Dunn Financial","East"],
+	["GMH","East"],
+	["LaSalle Alums","West"],
+	["Lacrosse International","West"],
+	["Mesa Fresh","East"],
 	["Mojo’s","West"],
-	["OPC","East"],
-	["Smokey Joes","East"]
+	["Penn Charter","West"],
+	["Smokey Joes","West"],
+	["Headstrong","East"]
+	["Springfield PW","East"]
 ],
 "games":
 [
-{"Date":"06/11/15","Games":[
-["Bryn Mawr Trust","Lacrosse International","6:30 PM"],
-["Dunn Financial","OPC","6:30 PM"],
-["GMH","1789","6:30 PM"],
-["Mesa Fresh","Smokey Joes","8:30 PM"],
-["Mojo’s","Bryn Mawr Trust","6:30 PM"]
-]},
-{"Date":"06/18/15","Games":[
-["Bryn Mawr Trust","OPC","6:30 PM"],
-["Dunn Financial","Mesa Fresh","6:30 PM"],
-["Mojo’s","GMH","6:30 PM"],
-["1789","LaSalle Alums","6:30 PM",6,14],
-["Smokey Joes","LaSalle Alums","6:30 PM"]
-]},
-{"Date":"06/25/15","Games":[
-["1789","Dunn Financial","6:30 PM",9,7],
-["Bryn Mawr Trust","GMH","6:30 PM",0,0],
-["Mojo’s","Mesa Fresh","6:30 PM",8,18],
-["LaSalle Alums","Smokey Joes","8:30 PM",0,0],
-["Lacrosse International","OPC","6:30 PM",0,0]
-]},
-{"Date":"07/09/15","Games":[
-	["GMH","Smokey Joes","8:30 PM",0,0],
-	["Bryn Mawr Trust","Mesa Fresh","6:30 PM",8,6],
-	["Mojo’s","Dunn Financial","6:30 PM",0,0],
-	["Lacrosse International","LaSalle Alums","6:30 PM",0,0],
-	["1789","OPC","6:30 PM",9,5]
-]},
-{"Date":"07/16/15","Games":[
-	["LaSalle Alums","Mesa Fresh","6:30 PM",0,0],
-	["Mojo’s","Bryn Mawr Trust","6:30 PM",0,0],
-	["Dunn Financial","GMH","6:30 PM",0,0],
-	["1789","Lacrosse International","6:30 PM",15,6],
-	["Smokey Joes","OPC","6:30 PM",0,0]
-]},
-{"Date":"07/23/15","Games":[
-["Mojo’s","Lacrosse International","6:30 PM",0,0],
-["Dunn Financial","Bryn Mawr Trust","6:30 PM",11,7],
-["GMH","Mesa Fresh","6:30 PM",0,0],
-["LaSalle Alums","OPC","6:30 PM",0,0],
-["1789","Smokey Joes","8:30 PM",0,0]
+{"Date":"06/09/16","Games":[
+	["BRYN MAWR TRUST","PENN CHARTER","6:30 PM",0,0],
+	["MOJO","DUNN FINANCIAL","6:30 PM",0,0],
+	["LAX INTERNATIONAL","GMH","8:00 PM",0,0]
+	["1789 GERMANTOWN ACADEMY","HEADSTRONG","6:30 PM",0,0]
+	["LASALLE ALUMNI","MESA FRESH","6:30 PM",0,0]
+	["SPRINGFIELD PW","SMOKEY JOES","8:00 PM",0,0]
+]},{"Date":"06/16/16","Games":[
+	["DUNN FINANCIAL","BRYN MAWR TRUST","6:30 PM",0,0],
+	["HEADSTRONG","GMH","8:00 PM",0,0],
+	["MESA FRESH","SPRINGFIELD PW","6:30 PM",0,0]
+	["LAX INTERNATIONAL","LASALLE ALUMNI","6:30 PM",0,0]
+	["MOJO","PENN CHARTER","6:30 PM",0,0]
+	["1789 GERMANTOWN ACADEMY","SMOKEY JOES","8:00 PM",0,0]
+]},{"Date":"06/23/16","Games":[
+	["BRYN MAWR TRUST","GMH","6:30 PM",0,0],
+	["DUNN FINANCIAL","MESA FRESH","6:30 PM",0,0],
+	["SPRINGFIELD PW","HEADSTRONG","6:30 PM",0,0]
+	["MOJO","LASALLE ALUMNI","6:30 PM",0,0]
+	["1789 GERMANTOWN ACADEMY","LAX INTERNATIONAL","6:30 PM",0,0]
+	["SMOKEY JOES","PENN CHARTER","6:30 PM",0,0]
+]},{"Date":"06/30/16","Games":[
+	["BRYN MAWR TRUST","HEADSTRONG","6:30 PM",0,0],
+	["SPRINGFIELD PW","DUNN FINANCIAL","6:30 PM",0,0],
+	["MESA FRESH","GMH","6:30 PM",0,0]
+	["LASALLE ALUMNI","PENN CHARTER","6:30 PM",0,0]
+	["1789 GERMANTOWN ACADEMY","MOJO","6:30 PM",0,0]
+	["LAX INTERNATIONAL","SMOKEY JOES","8:00 PM",0,0]
+]},{"Date":"07/07/16","Games":[
+	["BRYN MAWR TRUST","MESA FRESH","6:30 PM",0,0],
+	["DUNN FINANCIAL","HEADSTRONG","6:30 PM",0,0],
+	["SPRINGFIELD PW","GMH","6:30 PM",0,0]
+	["LASALLE ALUMNI","","6:30 PM",0,0]
+	["LAX INTERNATIONAL","PENN CHARTER","6:30 PM",0,0]
+	["SMOKEY JOES","MOJO","6:30 PM",0,0]
+]},{"Date":"07/14/16","Games":[
+	["SPRINGFIELD PW","BRYN MAWR TRUST","6:30 PM",0,0],
+	["DUNN FINANCIAL","GMH","6:30 PM",0,0],
+	["MESA FRESH","HEADSTRONG","8:00 PM",0,0]
+	["LASALLE ALUMNI","SMOKEY JOES","6:30 PM",0,0]
+	["LAX INTERNATIONAL","MOJO","6:30 PM",0,0]
+	["PENN CHARTER","1789 GERMANTOWN ACADEMY","8:00 PM",0,0]
 ]}
+
 ]}
