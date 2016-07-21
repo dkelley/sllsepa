7e3d6e342c20a4397059f1724dbefce42f5632cc
Daniel Kelley <dan@kelleyland.com>
2016-07-21 08:05:37 -0400

    latest scores

--- data/tuesdayDelco.js
+++ data/tuesdayDelco.js
@@ -13,38 +13,38 @@
 {"Date":"06/14/16","Games":[
 ["COCOS","O’Hara","6:30 PM",0,0],
 ["Haverford","Upper Darby","6:30 PM",16,3],
-["Springfield","HAVEN","6:30 PM",0,0],
-["Ridley","Old Lions","6:30 PM",0,0]
+["Springfield","HAVEN","6:30 PM",10,5],
+["Ridley","Old Lions","6:30 PM",8,6]
 ]},
 {"Date":"06/21/16","Games":[
 ["Haverford","COCOS","6:30 PM",0,0],
 ["HAVEN","O’Hara","6:30 PM",5,6],
-["Old Lions","Upper Darby","6:30 PM",7,9],
+["Old Lions","Upper Darby","6:30 PM",12,4],
 ["Ridley","Springfield","6:30 PM",0,0]
 ]},
 {"Date":"06/28/16","Games":[
 ["COCOS","HAVEN","6:30 PM",0,0],
-["O’Hara","Ridley","6:30 PM",0,20],
+["O’Hara","Ridley","6:30 PM",4,14],
 ["Old Lions","Haverford","7:30 PM",6,9],
 ["Upper Darby","Springfield","6:30 PM",0,0]
 ]},
 {"Date":"07/05/16","Games":[
 ["COCOS","Old Lions","6:30 PM",0,0],
 ["Springfield","Haverford","6:30 PM",3,13],
-["O’Hara","Upper Darby","6:30 PM",0,0],
-["HAVEN","Ridley","6:30 PM",0,0]
+["O’Hara","Upper Darby","6:30 PM",7,13],
+["HAVEN","Ridley","6:30 PM",6,8]
 ]},
 {"Date":"07/12/16","Games":[
 ["Ridley","COCOS","6:30 PM",0,0],
-["Springfield","Old Lions","6:30 PM",0,20],
+["Springfield","Old Lions","6:30 PM",6,8],
 ["Upper Darb","HAVEN","6:30 PM",0,0],
 ["Haverford","O’Hara","6:30 PM",14,3]
 ]},
 {"Date":"07/21/16","Games":[
-["Old Lions","O’Hara","6:30 PM",0,0],
+["Old Lions","O’Hara","6:30 PM",10,5],
 ["Springfield","COCOS","6:30 PM",0,20],
-["Upper Darby","Ridley","6:30 PM",0,0],
-["HAVEN","Haverford","7:30 PM",0,0]
+["Upper Darby","Ridley","6:30 PM",1,2],
+["HAVEN","Haverford","7:30 PM",3,10]
 ]
 }
 ]}
--- data/tuesdayWest.js
+++ data/tuesdayWest.js
@@ -25,26 +25,26 @@
 ]},
 {"Date":"06/28/16","Games":[
 ["Baggataway Tav","Garnet Valley","6:30 PM",3,7],
-["Great American Pub","Duffers Pub","8:00 PM",8,10],
-["Dunn Financial","Kelly’s Sports","6:30 PM",0,0],
+["Great American Pub","Duffers Pub","8:00 PM",11,12],
+["Dunn Financial","Kelly’s Sports","6:30 PM",5,12],
 ["PJ Whelihans","Rustin","6:30 PM",18,2]
 ]},
 {"Date":"07/05/16","Games":[
 ["Great American Pub","Baggataway Tav","6:30 PM",10,12],
 ["Kelly’s Sports","Garnet Valley","6:30 PM",3,15],
 ["PJ Whelihans","Duffers Pub","8:00 PM",8,11],
-["Rustin","Dunn Financial","6:30 PM",0,0]
+["Rustin","Dunn Financial","6:30 PM",5,16]
 ]},
 {"Date":"07/12/16","Games":[
 ["Baggataway Tav","Kelly’s Sports","6:30 PM",0,0],
 ["Great American Pub","PJ Whelihans","6:30 PM",6,8],
-["Rustin","Garnet Valley","6:30 PM",0,0],
+["Rustin","Garnet Valley","6:30 PM",4,12],
 ["Dunn Financial","Duffers Pub","8:00 PM",7,10]
 ]},
 {"Date":"07/19/16","Games":[
-["PJ Whelihans","Baggataway Tav","6:30 PM",0,0],
-["Kelly’s Sports","Rustin","6:30 PM",0,0],
-["Dunn Financial","Great American Pub","6:30 PM",0,0],
+["PJ Whelihans","Baggataway Tav","6:30 PM",9,8],
+["Kelly’s Sports","Rustin","6:30 PM",12,4],
+["Dunn Financial","Great American Pub","6:30 PM",9,14],
 ["Garnet Valley","Duffers Pub","8:00 PM",0,0]
 ]}
 ]}
